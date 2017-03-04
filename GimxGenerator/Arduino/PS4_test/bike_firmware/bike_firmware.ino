2e9111d4f4d552472d73e827b82876769c25072c
Mark H. Jones <markjones112358@gmail.com>
2017-03-05 06:21:13 +1300

    Removed old commented code

--- GimxGenerator/Arduino/PS4_test/bike_firmware/bike_firmware.ino
+++ GimxGenerator/Arduino/PS4_test/bike_firmware/bike_firmware.ino
@@ -52,8 +52,6 @@ uint8_t head_check[6] =  {0xf2, 0x03, 0x00, 0x00, 0x10, 0x00};
 uint8_t head_boot0[2] = {0x11, 0x01};
 uint8_t head_boot1[2] = {0x22, 0x01};
 uint8_t head_boot2[2] = {0x33, 0x01};
-// uint8_t resp_check_ready[16] = {0xf2, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x69, 0xed, 0x12};
-// uint8_t resp_check_wait[16] =  {0xf2, 0x01, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xcc, 0xe8, 0x43, 0x35};
 uint8_t head_reqst[6] = {0xf1, 0x03, 0x00, 0x00, 0x40, 0x00};
 uint8_t head_match = 0;
 uint8_t match_chall = 0;
@@ -66,12 +64,6 @@ bool tx_report_flag = 0;
 uint8_t main_state = 0;
 unsigned int tcnt2;
 
-// uint8_t chal1[64] {0xf0, 0x01, 0x00, 0x00, 0x4d, 0x6f, 0x72, 0xdb, 0x29, 0x5c, 0x8a, 0xf7, 0x0e, 0xe1, 0x55, 0x94, 0xed, 0x3f, 0x6c, 0xdb, 0xda, 0x6b, 0x16, 0xa2, 0xb2, 0xbe, 0x88, 0xa1, 0x3f, 0x48, 0x6e, 0xd4, 0x3d, 0xb6, 0x38, 0x59, 0xba, 0x04, 0xf3, 0xb4, 0x90, 0x6a, 0x76, 0xf6, 0xff, 0xb6, 0x17, 0x33, 0xb3, 0xea, 0xc7, 0x73, 0x59, 0xdf, 0xb7, 0x48, 0xbf, 0x0d, 0xbb, 0xbd, 0x96, 0xe0, 0xf6, 0x44};
-// uint8_t chal2[64] {0xf0, 0x01, 0x01, 0x00, 0x6f, 0x58, 0x1b, 0x0d, 0x5b, 0x41, 0xab, 0x36, 0x28, 0xd1, 0x89, 0xae, 0x8b, 0x3a, 0xf5, 0xe5, 0xf7, 0xd4, 0xd1, 0x28, 0xcc, 0x2b, 0x8b, 0xaa, 0xd9, 0x8d, 0xef, 0xca, 0x74, 0xff, 0x85, 0x96, 0x3e, 0x39, 0x44, 0x63, 0xe2, 0x6d, 0x1b, 0x59, 0x52, 0x80, 0x3c, 0x40, 0xf8, 0xfa, 0xc9, 0x80, 0xeb, 0xd7, 0x49, 0x01, 0x4d, 0x0d, 0x88, 0x36, 0xf7, 0xe8, 0xd3, 0x58};
-// uint8_t chal3[64] {0xf0, 0x01, 0x02, 0x00, 0x96, 0x33, 0x20, 0x06, 0xd0, 0x61, 0x6a, 0xc6, 0xe3, 0xac, 0xaa, 0xcb, 0x5a, 0xc2, 0x6b, 0x4b, 0xf9, 0x10, 0x8d, 0xc9, 0x5f, 0x7c, 0xb1, 0x12, 0x2d, 0x6b, 0x8e, 0x8a, 0x9e, 0x52, 0xdb, 0xd8, 0xc2, 0x24, 0x35, 0x0b, 0x0a, 0x2d, 0xd6, 0x65, 0x58, 0x0f, 0x09, 0x6b, 0xde, 0xf2, 0xba, 0x93, 0x6f, 0x52, 0xd4, 0xf7, 0xc8, 0xf7, 0x32, 0x01, 0xdb, 0xed, 0x68, 0x09};
-// uint8_t chal4[64] {0xf0, 0x01, 0x03, 0x00, 0x38, 0x1d, 0x31, 0xb2, 0x6e, 0xe3, 0x86, 0x0a, 0xbb, 0xe2, 0xd8, 0x3a, 0x0e, 0xd3, 0x35, 0x87, 0x52, 0x85, 0xd1, 0xe6, 0xb7, 0x25, 0x95, 0x2f, 0xf0, 0x3e, 0x60, 0x38, 0xd8, 0x97, 0xa5, 0x7f, 0x87, 0xd9, 0x4d, 0x05, 0xef, 0x47, 0xa0, 0x9f, 0x75, 0xd3, 0xe4, 0x58, 0xd4, 0x29, 0xa1, 0x23, 0xed, 0x19, 0x8a, 0x0d, 0xdf, 0x8f, 0xa5, 0x8c, 0xc3, 0xd3, 0x2c, 0x44};
-// uint8_t chal5[64] {0xf0, 0x01, 0x04, 0x00, 0x3e, 0xc4, 0x94, 0x25, 0x2e, 0xdc, 0xdf, 0xa1, 0x0a, 0x60, 0x44, 0x54, 0xfc, 0x84, 0x02, 0x3f, 0xe8, 0x78, 0xfd, 0xe5, 0xdf, 0x87, 0xbb, 0x71, 0x9a, 0x14, 0xd7, 0xaf, 0x27, 0x0b, 0x9c, 0x4b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1c, 0x7a, 0x32, 0xeb};
-// High when a value is ready to be read
 volatile int readFlag;
 
 
@@ -210,12 +202,9 @@ void setup() {
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
 #endif
   if (Usb.Init() == -1) {
-    // Serial.print(F("\r\nOSC did not start"));
     while (1); // Halt
   }
-  // Serial.print(F("\r\nPS4 USB Library Started"));
   b = 0;
-  // attachInterrupt(0, rising, RISING);
   Serial.begin(500000);
 }
 
@@ -259,15 +248,6 @@ bool matches_boot2(uint8_t byte_in) {
   return matches_head(byte_in, head_boot2, 2, match_boot2);
 }
 
-//Sets the millis value
-// void setMillis(unsigned long new_millis)
-// {
-//   uint8_t oldSREG = SREG;
-
-//   cli();
-//   timer0_millis = new_millis;
-//   SREG = oldSREG;
-// }
 
 void loop() {
 
@@ -360,9 +340,7 @@ void loop() {
           if (matches_boot2(byte_in)) {
             main_state = 6;
             last_rx = micros();
-            // setMillis(0);
             last_report_tx = micros();
-            // micros_adjust = micros();
             break;
           }
         }
@@ -382,17 +360,10 @@ void loop() {
           rx_counter++;
 
           if (rx_counter == 64) {
-            // PS4.setLed(Purple);
             // Weve received the whole challenge part
             auth_rx_flag = 0;
             rx_counter = 0;
             loop_count++;
-
-            // if (loop_count == 1) PS4.authenticate(64, &chal1[0]);
-            // else if (loop_count == 2) PS4.authenticate(64, &chal2[0]);
-            // else if (loop_count == 3) PS4.authenticate(64, &chal3[0]);
-            // else if (loop_count == 4) PS4.authenticate(64, &chal4[0]);
-            // else if (loop_count == 5) PS4.authenticate(64, &chal5[0]);
 
             if (ps4_out[2] == 0) PS4.setLed(0x000033);
             else if (ps4_out[2] == 1) PS4.setLed(0x000066);
@@ -400,7 +371,6 @@ void loop() {
             else if (ps4_out[2] == 3) PS4.setLed(0x0000CC);
             else if (ps4_out[2] == 4)  {
               PS4.setLed(0x0000FF);
-              // auth_state++;
             }
             PS4.authenticate(64, ps4_out);
 
@@ -411,35 +381,10 @@ void loop() {
           auth_rx_flag = 1;
           head_match = 0;
           rx_counter = 0;
-          // for (uint8_t i = 0; i < 64; i++) ps4_out[i] = Serial.read();
-          // PS4.authenticate(64, ps4_out);
-
-          // if (ps4_out[2] == 0) PS4.setLed(0x000033);
-          // else if (ps4_out[2] == 1) PS4.setLed(0x000066);
-          // else if (ps4_out[2] == 2) PS4.setLed(0x000099);
-          // else if (ps4_out[2] == 3) PS4.setLed(0x0000CC);
-          // else if (ps4_out[2] == 4)  {
-          //   PS4.setLed(0x0000FF);
-          //   auth_state++;
-          // }
         }
         else if ( matches_check(byte_in) ) {
-          // PS4.setLed(Yellow);
-          // if (toggle) {
-          //   // PS4.setLedFlash(10, 10);
-          //   toggle = 0;
-          // }
-          // else {
-          //   // PS4.setLedFlash(0, 0);
-          //   toggle = 1;
-          // }
-          PS4.authenticate2(16, check_response);
 
-          // noInterrupts();
-          // Serial.write(0x44);
-          // Serial.write(0x10);
-          // for (uint8_t i = 0; i < 16; i++) Serial.write(check_response[i]);
-          // Serial.write(check_response, 16);
+          PS4.authenticate2(16, check_response);
 
           if (check_response[2] == 0x00) PS4.setLed(Green);
           else if (check_response[2] == 0x10) PS4.setLed(Yellow);
@@ -451,39 +396,8 @@ void loop() {
           buffer_out_len = 18;
           buffer_out_flag = 1;
           buffer_out_micros = micros() + 1200;
-
-          // interrupts();
-          // Serial.flush();
-
-
-          // uint8_t ready_flag = 1;
-          // uint8_t wait_flag = 1;
-
-          // for (uint8_t i = 0; i < 12; i++) if (check_response[i] != resp_check_ready[i] || i != 1) ready_flag = 0;
-          // for (uint8_t i = 0; i < 12; i++) if (check_response[i] != resp_check_wait[i] || i != 1) wait_flag = 0;
-
-          // if (ready_flag) {
-          //   PS4.setLed(Green);
-          // }
-          // else if (wait_flag) {
-          //   PS4.setLed(Yellow);
-          // }
-          // else PS4.setLed(Red);
-
-          // // Check if the response is ready and update LED
-          // if (resp[0] == 0xf2 && resp[1] == 0x01 && resp[2] == 0x00) {
-          //   // auth_state = 2;
-          //   // send_count = 0;
-          //   PS4.setLed(Green);
-          // }
-          // else if (resp[0] == 0xf2 && resp[1] == 0x01 && resp[2] == 0x10) {
-          //   PS4.setLed(Yellow);
-          // }
-          // else PS4.setLed(Red);
         }
         else if (matches_reqst(byte_in)) {
-          // if (send_count % 2 == 0) PS4.setLed(0xFFFFFF); // Bright white
-          // else PS4.setLed(0x888888); // Dim white
 
           // Request response from DS4
           PS4.authenticate3(64, resp);
@@ -497,25 +411,6 @@ void loop() {
           buffer_out_len = 66;
           buffer_out_flag = 1;
           buffer_out_micros = micros() + 1200;
-          // Send response to PS4
-          // noInterrupts();
-          // Serial.write(0x44);
-          // Serial.write(0x40);
-          // Serial.write(resp, 64);
-          // for (uint8_t i = 0; i < 64; i++) {
-          //   Serial.write(resp[i]);
-          // }
-          // interrupts();
-          // Serial.flush();
-
-          // send_count++;
-
-          // if (resp[2] == 0x12) auth_rx_flag = 0;
-
-
-            // auth_state = 0;
-            // send_count = 0;
-            // PS4.setLed(Off);
         }
       }
 
@@ -577,18 +472,7 @@ void loop() {
 
       report[49] = 255;
       report[50] = 255 - PS4.getAnalogButton(L2);
-      // report[46] = 128;
-      // report[47] = 255 - PS4.getAnalogButton(R2);
 
-      // report[49] = 255 - PS4.getAnalogButton(L2);
-
-
-      // if (millis() >= last_tx + 5) {
-      //   for(int i = 0; i < report_len; i++) Serial.write(report[i]);
-      //   // }
-
-      //   last_tx = millis();
-      // }
     }
   }
 }
